0d3f2450f2e9b20c444c1d22979df99d2dd80af8
fedegioz <fedechn26@gmail.com>
2024-04-10 11:57:52 +0200

    Created branch

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: [
-            TextField(decoration: InputDecoration(hintText: "Cocktail Name", border: RoundedRectangleBorder(borderRadius: 10)),),
+            TextField(decoration: InputDecoration(hintText: "Cocktail Name", ),
             SizedBox(height: 10,),
             ElevatedButton(onPressed: () { searchCocktails(); },
             child: Text("Search"),)
